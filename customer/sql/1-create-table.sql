8790522390b26e5273fc88afc141cd7bf02b0ac9
Jonathan Hellgren <jonathan.hellgren7@gmail.com>
2023-05-18 12:15:09 +0200

    First commit, added customer/controller/repo and sql files.

---
+++ customer/sql/1-create-table.sql
@@ -0,0 +1,4 @@
+create table customer (id bigint not null, date_created datetime(6) not null, date_updated datetime(6), email varchar(255),
+first_name varchar(255), last_name varchar(255), phone varchar(255), ssn varchar(255), primary key (id)) engine=InnoDB;
+create table customer_seq (next_val bigint) engine=InnoDB;
+insert into customer_seq values ( 1 );
---
+++ customer/src/main/java/com/example/customer/CustomerController.java
@@ -0,0 +1,66 @@
+package com.example.customer;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerRepo customerRepo;
+    private static final Logger log = LoggerFactory.getLogger(CustomerController.class);
+
+    CustomerController(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    @RequestMapping
+    public List<Customer> getAllCustomers() {
+        log.info("All customers have been returned!");
+        return customerRepo.findAll();
+    }
+
+    @RequestMapping("/{id}")
+    public Customer findById(@PathVariable Long id) {
+        log.info("All customers matching the chosen ID have been returned!");
+        return customerRepo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
+    }
+
+    @RequestMapping("/{firstName}/firstName")
+    public List<Customer> customerByFirstName(@PathVariable String firstName) {
+        log.info("All customers matching the chosen firstname have been returned!");
+        return customerRepo.findByFirstName(firstName);
+    }
+
+    @RequestMapping("/{lastName}/lastName")
+    public List<Customer> customerByLastName(@PathVariable String lastName) {
+        log.info("All customers matching the chosen lastname have been returned!");
+        return customerRepo.findByLastName(lastName);
+    }
+
+    @RequestMapping("/Delete/{id}")
+    public String deleteCustomerById(@PathVariable Long id) {
+        customerRepo.deleteById(id);
+        log.info("Customer have been deleted by matching ID!");
+        return "Customer with id " + id + " have been deleted";
+    }
+
+    @PostMapping("/add")
+    public String addCustomer(@RequestBody Customer customer) {
+        customerRepo.save(customer);
+        log.info("Customer have been added");
+        return "Customer have been added";
+    }
+    @RequestMapping("/addWithParams")
+    public String addCustomerWithParams(@RequestParam String ssn, @RequestParam String firstName, @RequestParam String lastName,
+                                        @RequestParam String phone, @RequestParam String email) {
+        customerRepo.save(new Customer(ssn, firstName, lastName, phone, email));
+        log.info("Customer have been added");
+        return "Customer " + firstName + " " + lastName + " have been added";
+    }
+}
