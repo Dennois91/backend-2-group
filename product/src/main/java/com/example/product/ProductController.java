733d8876b4b194a16c81ff85e44b5bd23a597dd1
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-05-29 15:44:04 +0200

    Errorhandling completed

--- product/src/main/java/com/example/product/ProductController.java
+++ product/src/main/java/com/example/product/ProductController.java
@@ -6,7 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 
 import java.util.List;
@@ -21,11 +26,13 @@ public class ProductController {
         this.repo = repo;
     }
 
+    @Retryable
     @RequestMapping
     public List<Product> getAllProducts() {
         return repo.findAll();
     }
 
+    @Retryable
     @RequestMapping("/{id}")
     public Product findById(@PathVariable Long id) {
         log.info("All products matching the chosen ID have been returned!");
@@ -33,6 +40,7 @@ public class ProductController {
                 .orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
     }
 
+    @Retryable
     @PostMapping("/add")
     public ResponseEntity<String> addProduct(@Valid @RequestBody Product p) {
         String send;
@@ -52,6 +60,7 @@ public class ProductController {
         }
     }
 
+    @Retryable
     @RequestMapping("/delete/{id}")
     public String deleteProductById(@PathVariable("id") Long id) {
         if (repo.findById(id).isPresent()) {
@@ -61,5 +70,31 @@ public class ProductController {
             return "Deleted: " + p;
         }
         return "Product with id= " + id + " not found";
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + HttpStatus.INTERNAL_SERVER_ERROR
+                + " - Product with the specified ID does not exist!" + " Id used = " + e.getMessage());
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request. Please check the URL.");
+    }
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    public ResponseEntity<String> handleBadRequestException(HttpClientErrorException.BadRequest e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("BadRequest" + e.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request. Please check the URL.");
+    }
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request. Please check the URL.");
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred.");
     }
 }
